ec8d30ece84cdd52faf9bb4238c582384761685f
relasts <tsovyan@inbox.ru>
2024-06-03 08:20:57 +0300

    home 1

--- babloss/settings.py
+++ babloss/settings.py
@@ -74,26 +74,26 @@ WSGI_APPLICATION = 'babloss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'babloss',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin12345',
-#         'HOST': 'localhost',
-#         'PORT': '5433'
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test4',
-	'USER': 'postgres',
-	'PASSWORD': '1234',
-	'HOST': 'localhost',
-	'PORT': 5432
+        'NAME': 'babloss',
+        'USER': 'postgres',
+        'PASSWORD': 'admin12345',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'test4',
+# 	'USER': 'postgres',
+# 	'PASSWORD': '1234',
+# 	'HOST': 'localhost',
+# 	'PORT': 5432
+#     }
+# }
 
 
 # Password validation
