cf08f92f797ddfdf325cef23105f7cf0cf8aa478
relasts <tsovyan@inbox.ru>
2024-06-27 12:47:10 +0300

    ср

--- app/front_app/views.py
+++ app/front_app/views.py
@@ -244,9 +244,10 @@ def viewAnalytic(request, id=None):
     if datas != 'Error':
         for key in datas[0].keys():
             if datas[0][key] != []:
-                icos = getColorImg([obj['img'] for obj in datas[1][key]])
-                for obj, col in zip(datas[0][key], icos):
-                    obj['color'] = col
+                if datas[0][key][0]['img'] != 'None':
+                    icos = getColorImg([obj['img'] for obj in datas[1][key]])
+                    for obj, col in zip(datas[0][key], icos):
+                        obj['color'] = col
 
     if userData['role'] == 'enterprise':
         nav1 = [['analytic', 'аналитика']]
